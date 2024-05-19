8fa031bed76e4bf362198ae408b15927627dc224
Kez Anwar <kezanwar@gmail.com>
2024-05-19 20:05:40 +0100

    feature/notifications - added local notifications

--- src/hocs/notifications/Notifications.tsx
+++ src/hocs/notifications/Notifications.tsx
@@ -90,6 +90,23 @@ const Notifications: FC<Props> = ({ children }) => {
       (response) => dispatch(setNotification(response.notification))
     );
 
+    Notify.getAllScheduledNotificationsAsync()
+      .then((scheduled) => {
+        if (!scheduled.length) {
+          Notify.scheduleNotificationAsync({
+            content: {
+              title: "Foodie",
+              body: "Checkout deals in your area!",
+              data: { data: "goes here" },
+            },
+            trigger: { seconds: 60 * 10, repeats: true },
+          })
+            .then()
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+
     return () => {
       //   Notify.removeNotificationSubscription(notificationListener.current!);
       Notify.removeNotificationSubscription(responseListener.current!);
